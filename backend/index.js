1c519b2840486383326505c46ce0f3eea62c290d
MaxiBelli <maximiliano_belli@hotmail.com>
2023-07-04 10:59:54 -0300

    feat: Connect App to MongoDB database

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,15 @@
 import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
 
 const app = express();
 
-app.listen(4000, () => {
-    console.log("Server running on port 4000")
+dotenv.config();
+
+connectDB();
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
 })
