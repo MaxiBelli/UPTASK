95b33a3b3ddebddb7800349fa9c79c2a1b0f8055
MaxiBelli <maximiliano_belli@hotmail.com>
2023-07-05 15:57:54 -0300

    feat: Generate unique ID for token

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,5 @@
 import User from "../models/User.js";
+import generateId from "../helpers/generateId.js";
 
 const register = async (req, res) => {
   // Avoid duplicate registrations
@@ -12,6 +13,7 @@ const register = async (req, res) => {
 
   try {
     const user = new User(req.body);
+    user.token = generateId();//Generate unique ID for token
     const storedUser = await user.save();
 
     res.json(storedUser);
