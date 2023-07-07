7637c5d4939cc222312fe2e7a278f576e4e550c1
MaxiBelli <maximiliano_belli@hotmail.com>
2023-07-07 09:04:05 -0300

    feat: Create Projects

--- backend/controllers/projectController.js
+++ backend/controllers/projectController.js
@@ -3,7 +3,17 @@ import User from "../models/User.js";
 
 const getProjects = async (req, res) => {};
 
-const createProject = async (req, res) => {};
+const createProject = async (req, res) => {
+  const project = new Project(req.body);
+  project.creator = req.user._id;
+
+  try {
+    const storedProject = await project.save();
+    res.json(storedProject);
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 const getProject = async (req, res) => {};
 
