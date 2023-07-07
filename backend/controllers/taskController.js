193bad07d4265655227dc1602a2611515d36dcec
MaxiBelli <maximiliano_belli@hotmail.com>
2023-07-07 11:20:59 -0300

    feat: Add Tasks to Existing Project

--- backend/controllers/taskController.js
+++ backend/controllers/taskController.js
@@ -1,7 +1,28 @@
 import Project from "../models/Project.js";
 import Task from "../models/Task.js";
 
-const addTask = async (req, res) => {};
+const addTask = async (req, res) => {
+  const { project } = req.body;
+
+  const existsProject = await Project.findById(project);
+
+  if (!existsProject) {
+    const error = new Error("The Project does not exist");
+    return res.status(404).json({ msg: error.message });
+  }
+
+  if (existsProject.creator.toString() !== req.user._id.toString()) {
+    const error = new Error("You don't have permission to add tasks");
+    return res.status(403).json({ msg: error.message });
+  }
+
+  try {
+    const storedTask = await Task.create(req.body);
+    res.json(storedTask);
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 const getTask = async (req, res) => {};
 
