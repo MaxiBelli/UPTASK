3b43bde38b7f8d81cac3599416a3ffb38d27cafb
MaxiBelli <maximiliano_belli@hotmail.com>
2023-07-07 09:30:03 -0300

    feat: Get and Validate Project by ID

--- backend/controllers/projectController.js
+++ backend/controllers/projectController.js
@@ -18,7 +18,23 @@ const createProject = async (req, res) => {
   }
 };
 
-const getProject = async (req, res) => {};
+const getProject = async (req, res) => {
+  const { id } = req.params;
+
+  const project = await Project.findById(id);
+
+  if (!project) {
+    const error = new Error("Not Found");
+    return res.status(404).json({ msg: error.message });
+  }
+
+  if (project.creator.toString() !== req.user._id.toString()) {
+    const error = new Error("Invalid Action");
+    return res.status(401).json({ msg: error.message });
+  }
+
+  res.json(project);
+};
 
 const editProject = async (req, res) => {};
 
