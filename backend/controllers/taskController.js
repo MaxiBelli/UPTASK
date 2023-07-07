65cf4b337eba8bc745014094347793e31b482d06
MaxiBelli <maximiliano_belli@hotmail.com>
2023-07-07 10:42:16 -0300

    feat: Task Routing and Functions

---
+++ backend/controllers/taskController.js
@@ -0,0 +1,14 @@
+import Project from "../models/Project.js";
+import Task from "../models/Task.js";
+
+const addTask = async (req, res) => {};
+
+const getTask = async (req, res) => {};
+
+const updateTask = async (req, res) => {};
+
+const deleteTask = async (req, res) => {};
+
+const changeStatus = async (req, res) => {};
+
+export { addTask, getTask, updateTask, deleteTask, changeStatus };
