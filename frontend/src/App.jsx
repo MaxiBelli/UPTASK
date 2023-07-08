dd00398c9e7944bbb1f75d190987237044d62d3f
MaxiBelli <maximiliano_belli@hotmail.com>
2023-07-08 18:16:38 -0300

    feat: Create Routing with React Router DOM 6

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,25 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import AuthLayout from "./layouts/AuthLayout";
+import Login from "./pages/auth/Login";
+import Register from "./pages/auth/Register";
+import ForgotPassword from "./pages/auth/forgotPassword";
+import NewPassword from "./pages/auth/NewPassword";
+import ConfirmAccount from "./pages/auth/ConfirmAccount";
+
 function App() {
   return (
-    <div className="App">
-      <h1>UpTask</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<AuthLayout />}>
+          <Route index element={<Login />} />
+          <Route path="register" element={<Register />} />
+          <Route path="forgot-password" element={<ForgotPassword />} />
+          <Route path="forgot-password/:token" element={<NewPassword />} />
+          <Route path="confirm/:id" element={<ConfirmAccount />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
