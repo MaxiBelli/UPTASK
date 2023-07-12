4d06aa0bb1e5fcf030d360f5c26745d61d148919
MaxiBelli <maximiliano_belli@hotmail.com>
2023-07-12 10:03:28 -0300

    feat: Validate Token for User Authentication

--- frontend/src/auth/context/AuthProvider.jsx
+++ frontend/src/auth/context/AuthProvider.jsx
@@ -1,11 +1,36 @@
 import { useState, useEffect, createContext } from "react";
 import { useNavigate } from "react-router-dom";
-import clienteAxios from "../config/clienteAxios";
+import clientAxios from "../../config/clientAxios";
 
 const AuthContext = createContext();
 
 const AuthProvider = ({ children }) => {
   const [auth, setAuth] = useState({});
+
+  useEffect(() => {
+    const authenticateUser = async () => {
+      const token = localStorage.getItem("token");
+      if (!token) {
+        return;
+      }
+
+      const config = {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      };
+
+      try {
+        const { data } = await clientAxios.get("/users/profile", config);
+        setAuth(data);
+      } catch (error) {
+        setAuth({});
+      }
+    };
+
+    authenticateUser();
+  }, []);
 
   return (
     <AuthContext.Provider
