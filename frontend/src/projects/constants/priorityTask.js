14c6f759344fc19a0922b12a3cb556cf700f779d
MaxiBelli <maximiliano_belli@hotmail.com>
2023-08-01 18:45:02 -0300

    feat: Add Task Priority Constants

---
+++ frontend/src/projects/constants/priorityTask.js
@@ -0,0 +1,7 @@
+export const PRIORITY = [
+    { name: "Low", bgColor: "bg-green-500" },
+    { name: "Medium", bgColor: "bg-yellow-500" },
+    { name: "High", bgColor: "bg-red-500" },
+  ];
+  
+ 
