e991747b26ec1f2dbcca36b06ea3bcfa0365fe96
MaxiBelli <maximiliano_belli@hotmail.com>
2023-08-03 16:54:23 -0300

    feat: Show Collaborator Legend for Non-Owned Projects

--- frontend/src/projects/components/ProjectPreview.jsx
+++ frontend/src/projects/components/ProjectPreview.jsx
@@ -1,8 +1,11 @@
 import { Link } from "react-router-dom";
+import useAuth from "../../auth/hooks/useAuth";
 import SVGIcons from "../../assets/icons/SVGIcons";
 
 const ProjectPreview = ({ project }) => {
-  const { name, _id, client } = project;
+  const auth = useAuth();
+
+  const { name, _id, client, creator } = project;
 
   return (
     <div className="border-b p-5 flex flex-col md:flex-row justify-between">
@@ -14,6 +17,12 @@ const ProjectPreview = ({ project }) => {
             {""} {client}
           </span>
         </p>
+
+        {auth._id !== creator && (
+          <p className="p-1 text-sm rounded-lg text-white bg-green-500 font-bold uppercase">
+            Collaborator
+          </p>
+        )}
       </div>
 
       <Link
